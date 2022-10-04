f5367dc26baf5efeadc1ef14dcabb36ed2e50b52
Fletcher Blight <fletcher.blight@gmail.com>
2022-10-04 19:24:00 +1000

    Triangle

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,6 @@ fn main()
   let _gl_context = window.gl_create_context().unwrap();
   let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
-  unsafe {
-    gl::Viewport(0, 0, 900, 700); // set viewport
-    gl::ClearColor(0.3, 0.3, 0.5, 1.0);
-  }
-
   let vert_shader = render_gl::Shader::from_vert_source(
     &CString::new(include_str!("triangle.vert")).unwrap()
   ).unwrap();
@@ -38,7 +33,52 @@ fn main()
   ).unwrap();
 
   let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
-  shader_program.set_used();
+
+  let vertices: Vec<f32> = vec![
+    -0.5, -0.5, 0.0,
+    0.5, -0.5, 0.0,
+    0.0, 0.5, 0.0,
+  ];
+
+  let mut vbo: gl::types::GLuint = 0;
+  unsafe {
+    gl::GenBuffers(1, &mut vbo);
+  }
+  unsafe {
+    gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+    gl::BufferData(
+      gl::ARRAY_BUFFER,
+      (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+      vertices.as_ptr() as *const gl::types::GLvoid,
+      gl::STATIC_DRAW
+    );
+    gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+  }
+
+  let mut vao: gl::types::GLuint = 0;
+  unsafe {
+    gl::GenVertexArrays(1, &mut vao);
+  }
+  unsafe {
+    gl::BindVertexArray(vao);
+    gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+    gl::EnableVertexAttribArray(0);
+    gl::VertexAttribPointer(
+      0, // index of generic vertex attribute ("layout (location = 0)")
+      3, // the number of components per generic vertex attribute
+      gl::FLOAT,
+      gl::FALSE, // normalized (int-to-float conversion)
+      (3 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+      std::ptr::null() // offset of first component
+    );
+    gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    gl::BindVertexArray(0);
+  }
+
+  unsafe {
+    gl::Viewport(0, 0, window.size().0 as gl::types::GLint, window.size().1 as gl::types::GLint);
+    gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+  }
 
   let mut event_pump = sdl.event_pump().unwrap();
   'main: loop {
@@ -51,6 +91,12 @@ fn main()
 
     unsafe {
       gl::Clear(gl::COLOR_BUFFER_BIT);
+    }
+
+    shader_program.set_used();
+    unsafe {
+      gl::BindVertexArray(vao);
+      gl::DrawArrays(gl::TRIANGLES, 0, 3);
     }
     window.gl_swap_window();
   }
