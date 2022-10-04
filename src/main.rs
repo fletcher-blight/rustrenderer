8327989c07a319705c3a5228dab737ecd474d7b3
Fletcher Blight <fletcher.blight@gmail.com>
2022-10-04 15:43:17 +1000

    Video Subsystem

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 extern crate sdl2;
 
 fn main() {
-  let _sdl = sdl2::init().unwrap();
+  let sdl = sdl2::init().unwrap();
+  let video_subsystem = sdl.video().unwrap();
+  let _window = video_subsystem
+    .window("Rust Renderer", 640, 480)
+    .resizable()
+    .build()
+    .unwrap();
 }
