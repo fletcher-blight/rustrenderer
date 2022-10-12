55ec2ad780f6cf6d773f290e79e1732b8eda68e5
Fletcher Blight <fletcher.blight@gmail.com>
2022-10-12 14:07:14 +1000

    a window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate gl;
 use gl::types::*;
+use sdl2::event::Event;
 use sdl2::video::GLProfile;
 
 fn main() -> Result<(), String> {
@@ -20,6 +21,22 @@ fn main() -> Result<(), String> {
     let _gl_context = window.gl_create_context()?;
     let _gl =
         gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe { gl::ClearColor(0.2, 0.5, 0.9, 1.0) };
+
+    let mut event_pump = sdl.event_pump()?;
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'main,
+                _ => (),
+            }
+        }
+
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+
+        window.gl_swap_window();
+    }
 
     Ok(())
 }
